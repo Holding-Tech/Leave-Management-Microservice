b891636c35096a332a011acdbdc1e9e909a056ea
katleho <hulang606@gmail.com>
2025-06-08 15:57:15 +0200

    Update Status Method
    Update Status Method

--- Leave_Application/Leave_Application/obj/Debug/net8.0/Leave_Application.AssemblyInfo.cs
+++ Leave_Application/Leave_Application/obj/Debug/net8.0/Leave_Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leave_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2c95192c49ad0e5ba43119c9dfef8acde7137c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e004124c60c0fb8ef9134c6bda2042a3f9d5de91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leave_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leave_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
