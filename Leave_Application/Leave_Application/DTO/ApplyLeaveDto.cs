6f3a76d31e5162f326e2541e7955fdfe22424c44
Katleho06Molefe <Hulang606@gmail.com>
2025-06-11 14:21:46 +0100

    LeaveApprovalDto
    LeaveApprovalDto

--- Leave_Application/Leave_Application/DTO/ApplyLeaveDto.cs
+++ Leave_Application/Leave_Application/DTO/ApplyLeaveDto.cs
@@ -2,5 +2,11 @@
 {
     public class ApplyLeaveDto
     {
+        public int EmployeeId { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; }
+        public string? SupportingDocPath { get; set; }
     }
 }
--- Leave_Application/Leave_Application/obj/Debug/net8.0/Leave_Application.AssemblyInfo.cs
+++ Leave_Application/Leave_Application/obj/Debug/net8.0/Leave_Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leave_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6187a95d79532dbfab054d8fcafcd447457b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd8e0fffa4dbd3c45c981dfe038c1bdeae0ccab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leave_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leave_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
